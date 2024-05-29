569caed7e16555407fcca545547adf7334fc3afd
Parthiv Sadhukhan <parthivsadhukhan@gmail.com>
2024-05-29 14:23:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,4 +10,19 @@ import zipfile
 
 # Page title
 st.set_page_config(page_title='Signature Verify', page_icon='🤖')
-st.title('🤖 ML Model Building')
+
+st.title("Enter Sign")
+
+input_method = st.radio("Select input method:", ("Upload from Drive", "Take a picture"))
+
+# File uploader widget
+if input_method == "Upload from Drive":
+    uploaded_image = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+    if uploaded_image is not None:
+        st.image(uploaded_image, caption="Uploaded Image", use_column_width=True)
+
+# Camera input widget
+elif input_method == "Take a picture":
+    captured_image = st.camera_input("Take a picture")
+    if captured_image is not None:
+        st.image(captured_image, caption="Captured Image", use_column_width=True)
